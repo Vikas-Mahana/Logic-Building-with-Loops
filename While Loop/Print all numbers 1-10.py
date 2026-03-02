1bfdfcbb2e72d9e0e4bc8f98e62d226eb1b299ed
Vikas Mahana <maharanavikas2@gmail.com>
2026-03-02 22:25:06 +0530

    print 1-10

---
+++ While Loop/Print all numbers 1-10.py
@@ -0,0 +1,8 @@
+""" Print all numbers from 1 to 10 using a loop. """
+
+# Initialize the variable
+number = 1
+# Use a while loop to print numbers from 1 to 10
+while number <= 10:
+    print(number)
+    number += 1  # Increment the number by 1
