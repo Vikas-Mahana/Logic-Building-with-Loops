3cc4b125d0755bdbeb47fdbdce29977590431543
Vikas Mahana <maharanavikas2@gmail.com>
2026-03-02 22:33:05 +0530

    changes

--- While Loop/Print the multiplication table.py
+++ While Loop/Print the multiplication table.py
@@ -1,4 +1,6 @@
 """ Print the multiplication table of a given number from n × 1 to n × 10. """
+
+
 # Get the number from the user
 number = int(input("Enter a number to print its multiplication table: "))
 # Initialize the multiplier
@@ -8,4 +10,3 @@ while multiplier <= 10:
     result = number * multiplier
     print(f"{number} × {multiplier} = {result}")
     multiplier += 1  # Increment the multiplier by 110
-    
