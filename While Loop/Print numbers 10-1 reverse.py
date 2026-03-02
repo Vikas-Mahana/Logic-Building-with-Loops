104e503685f11c089823778871988a94617172f0
Vikas Mahana <maharanavikas2@gmail.com>
2026-03-02 22:26:56 +0530

    print 10-1

---
+++ While Loop/Print numbers 10-1 reverse.py
@@ -0,0 +1,7 @@
+""" Print numbers from 10 down to 1 in reverse order. """
+
+# Initialize the variable
+number = 10
+while number >= 1:
+    print(number)
+    number -= 1  # Decrement the number by 1
