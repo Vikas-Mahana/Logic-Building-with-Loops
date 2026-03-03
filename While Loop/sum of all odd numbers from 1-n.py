6f3b7dd86959018ccc0d2309f060f0f76dcd9e4a
Vikas Mahana <maharanavikas2@gmail.com>
2026-03-03 22:00:41 +0530

    sum of odd no.s 1 to n

---
+++ While Loop/sum of all odd numbers from 1-n.py
@@ -0,0 +1,14 @@
+""" Calculate the sum of all odd numbers from 1 up to n. """
+
+num = int(input("Enter a positive number: "))
+# Initialize the variable
+n = 1
+sum = 0
+# Use a while loop to print numbers from 1 to num
+while n <= num:
+    if n % 2 != 0:
+        print(n)
+        sum += n
+    n +=1 #incremented by 1
+
+print(sum)
